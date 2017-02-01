19f6a3f7165fb88e01e8791ae4582fb1b56b5f13
Ruben Gil <gil.r419@gmail.com>
2017-02-01 18:14:54 -0500

    add active column

--- backend/models/document-model.js
+++ backend/models/document-model.js
@@ -34,6 +34,13 @@ module.exports = function(sequelize, DataTypes) {
       validate: {
         notNull: true
       }
+    },
+    active: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: true,
+      validate: {
+        notNull: true
+      }
     }
   }, {
     classMethods: {
