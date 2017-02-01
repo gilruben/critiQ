e10c43f165bfd74795d5f593287218440fc8deca
Crazy Kiwi <Nasita-Haque>
2017-02-01 12:12:47 -0500

    add comment model close #5

---
+++ backend/migrations/20170201165232-create-comment.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      comment: {
+        type: Sequelize.STRING
+      },
+      textLocation: {
+        type: Sequelize.OBJECT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Comments');
+  }
+};
---
+++ backend/models/comment-model.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Comment = sequelize.define('Comment', {
+    comment: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+        notNull: true
+      }
+    },
+    textLocation: {
+      type: DataTypes.JSON,
+      validate: {
+        notEmpty: true
+      }
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Comment.belongsTo(models.User, {
+          onDelete: "CASCADE", 
+          foreignKey: {
+            allowNull: false
+          }
+        })
+        Comment.belongsTo(models.Document, {
+          onDelete: "CASCADE", 
+          foreignKey: {
+            allowNull: false
+          }
+        })
+      }
+    }
+  });
+  return Comment;
+};
