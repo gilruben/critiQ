b99c3c14acb4c51cf152f44540204fa08e1a2427
Ruben Gil <gil.r419@gmail.com>
2017-02-02 17:22:00 -0500

    create route and test to edit user data

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/users', require('./user-router'));
-// router.use('/documents', require('./document-router'));
-// router.use('/comments', require('./comment-router'));
+// router.use('/document', require('./document-router'));
+// router.use('/comment', require('./comment-router'));
 
 module.exports = router;
