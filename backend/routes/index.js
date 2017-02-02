99c1b2cc5dee77a7e6ef159d395b33c31adde0e0
jondubya86 <jonathanwu86@gmail.com>
2017-02-02 17:45:09 -0500

    fix conflict in routes/index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,12 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/users', require('./user-router'));
-<<<<<<< HEAD
 router.use('/documents', require('./document-router'));
 // router.use('/comments', require('./comment-router'));
-=======
-// router.use('/document', require('./document-router'));
-// router.use('/comment', require('./comment-router'));
->>>>>>> 39b3308c591959201ea1af2f9a90e940fc901bec
 
 module.exports = router;
