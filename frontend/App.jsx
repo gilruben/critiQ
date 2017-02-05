3bab24b5e30102b69da32b8a801c66eda153eda4
Carlos Martinez <martinezcj2@gmail.com>
2017-02-05 14:21:36 -0500

    rename style folder

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 
 import LandingPage from './components/landingPage/LandingPage';
 
-import './css/main.css';
+import './styles/main.css';
 
 const App = props => (
   <div>
