8aeb6d2150886e1fc4d83aaee08cf32bb75e73ad
Carlos Martinez <martinezcj2@gmail.com>
2017-02-06 12:15:47 -0500

    move state from landing page to sign in

--- frontend/components/landingPage/LandingPage.jsx
+++ frontend/components/landingPage/LandingPage.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import SignIn from './SignIn';
 
 const LandingPage = React.createClass({
-  getInitialState() {
-    return { email: '', password: '' };
-  },
   render() {
     return (
       <div className="landing-page">
--- frontend/components/landingPage/SignIn.jsx
+++ frontend/components/landingPage/SignIn.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const SignIn = React.createClass({
+  getInitialState() {
+    return { email: '', password: '' };
+  },
   render() {
     return (
       <div className="sign-in">
         <h1>Sign in</h1>
         <form>
-          <input type="text" placeholder="username" />
-          <input type="password" placeholder="password" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
           <input type="submit" />
         </form>
         <p>Don{"'"}t have account? <span>Sign up</span></p>
