8a515835ed9ed29a14f4c9fe908be56d4b5f2537
jondubya86 <jonathanwu86@gmail.com>
2017-02-08 18:26:57 -0500

    passport requires edits

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -1,4 +1,8 @@
 'use strict';
+
+const bcrypt = require('bcrypt-nodejs');
+const passport = require('passport');
+
 module.exports = function(sequelize, DataTypes) {
   const User = sequelize.define('User', {
     username: {
@@ -21,25 +25,56 @@ module.exports = function(sequelize, DataTypes) {
         isEmail: true,
       },
     },
-    bio: DataTypes.TEXT,
-    rating: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-      validate: {
-        min: 0,
+    // bio: DataTypes.TEXT,
+    // rating: {
+    //   type: DataTypes.INTEGER,
+    //   defaultValue: 0,
+    //   validate: {
+    //     min: 0,
+    //   },
+    // },
+    // level: {
+    //   type: DataTypes.STRING,
+    //   validate: {
+    //     notEmpty: true,
+    //   },
+    // },
+    // reviewerStatus: {
+    //   type: DataTypes.BOOLEAN,
+    //   defaultValue: true,
+    // },
+  },
+    {
+      hooks: {
+        beforeCreate: function(user, options, next) {
+          bcrypt.genSalt(8, function(err, salt){
+            if(err){
+              throw err
+            }
+            bcrypt.hash(user.password, salt, null, function(ele, hash){
+              if(err){
+                throw err
+              }
+              console.log(user.password, hash)
+              user.password=hash
+              next()
+            })
+          })
+        },
       },
     },
-    level: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: true,
-      },
+    {
+    comparePassword: function(potentialpass, cb){
+      bcrypt.compare(potentialpass, this.password, function(err, ismatch){
+        if(err){
+          throw err
+        }else{
+          cb(null, ismatch)
+        }
+      })
+    }
     },
-    reviewerStatus: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true,
-    },
-  }, {
+    {
     classMethods: {
       associate: function (models) {
         User.hasMany(models.Document);
@@ -47,6 +82,6 @@ module.exports = function(sequelize, DataTypes) {
         User.hasMany(models.Comment);
       },
     },
-  });
+    });
   return User;
 };
