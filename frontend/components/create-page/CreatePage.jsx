39af4467b249ec80a4b9031aeaa77662770b400e
Crazy Kiwi <Nasita-Haque>
2017-02-08 14:09:35 -0500

    add inline style to component

--- frontend/components/create-page/CreatePage.jsx
+++ frontend/components/create-page/CreatePage.jsx
@@ -7,31 +7,10 @@ const CreatePage = React.createClass({
     return { title: '', body: {}, category: 'paper', privacy: '', deadline: '', editorState: EditorState.createEmpty() };
   },
   onChange(editorState) {
-    return this.setState({ editorState: editorState });
+    return this.setState({ editorState });
   },
-  // onBoldClick() {
-  //   this.onChange(
-  //     RichUtils.toggleInlineStyle(
-  //       this.state.editorState,
-  //       'BOLD',
-  //   ));
-  // }, 
-  // onItalicClick() {
-  //   this.onChange(
-  //     RichUtils.toggleInlineStyle(
-  //       this.state.editorState,
-  //       'ITALIC',
-  //   ));
-  // }, 
-  // onUnderlineClick() {
-  //   this.onChange(
-  //     RichUtils.toggleInlineStyle(
-  //       this.state.editorState,
-  //       'UNDERLINE',
-  //   ));
-  // },
-  onDecorationClick(input, decoration) {
-    if(input === 'BOLD') {
+  onDecorationClick(input) {
+    if (input === 'BOLD') {
       this.onChange(
       RichUtils.toggleInlineStyle(
         this.state.editorState,
@@ -54,37 +33,54 @@ const CreatePage = React.createClass({
   onUploadClick() {
     const document = this.state.editorState.getCurrentContent();
     const rawData = convertToRaw(document);
-    console.log(rawData);
+    console.log('rawData =>', rawData);
+  },
+  addTempState(inputName, event) {
+    this.setState({ [inputName]: event.target.value });
   },
   render() {
+    const decoration = [
+    { name: 'Bold', style: 'BOLD' },
+    { name: 'Italic', style: 'ITALIC' },
+    { name: 'Underline', style: 'UNDERLINE' },
+    ];
+
     return (
       <div>
         <h1>Uploads</h1>
         <div>
           <form>
             <h3>Title:</h3>
-            <input type="text" placeholder="Enter a title"></input>
+            <input type="text" placeholder="Enter a title" onChange={this.addTempState.bind(this, 'title')} />
             <h3>Privacy Setting:</h3>
-            <select>
+            <select onChange={this.addTempState.bind(this, 'privacy')}>
               <option value="public">public</option>
               <option value="semi-private">semi-private</option>
               <option value="private">private</option>
             </select>
             <h3>Category</h3>
-            <select>
+            <select onChange={this.addTempState.bind(this, 'category')}>
               <option value="paper">paper</option>
               <option value="cover letter">cover letter</option>
               <option value="resume">resume</option>
               <option value="other writings">other writings</option>
             </select>
             <h3>Deadline:</h3>
-            <input name="date" type="date"></input> 
+            <input name="date" type="date" onChange={this.addTempState.bind(this, 'deadline')} />
           </form>
-          <button onClick={this.onDecorationClick.bind(this, 'BOLD') }>Bold</button>
-          <button onClick={this.onDecorationClick.bind(this, 'ITALIC') }>Italic</button>
-          <button onClick={this.onDecorationClick.bind(this, 'UNDERLINE') }>Underline</button>
+          {decoration.map((val, idx) => {
+            return (
+              <button
+                key={idx} 
+                onClick={this.onDecorationClick.bind(this, val.style)}>
+                { val.name }
+              </button>
+            )})}
           <div className="editor">
-            <Editor editorState={this.state.editorState} onChange={this.onChange} />
+            <Editor
+              editorState={this.state.editorState}
+              onChange={this.onChange}
+            />
           </div>
         </div>
         <button onClick={this.onUploadClick}>Upload</button>
