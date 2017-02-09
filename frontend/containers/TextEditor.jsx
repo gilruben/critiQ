1433d5348eddef6e0fbe88486b941c40adb7c0ad
Ruben Gil <gil.r419@gmail.com>
2017-02-09 17:39:53 -0500

    add functionality to create entitys on highlighted text

--- frontend/containers/TextEditor.jsx
+++ frontend/containers/TextEditor.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Editor, EditorState, RichUtils, Modifier, convertToRaw, convertFromRaw } from 'draft-js';
+import { Editor, EditorState, RichUtils, Modifier, CompositeDecorator, convertToRaw, convertFromRaw } from 'draft-js';
 import { Record } from 'immutable';
+import SelectedText from '../components/SelectedText';
 import '../styles/text-editor.css';
 
 const TextEditor = React.createClass({
   getInitialState() {
-    return { editorState: EditorState.createEmpty() };
+    // Applies the SelectedText component to text that has been highlighted
+    // and labeled as a COMMENT entity
+    const decorator = new CompositeDecorator([
+      {
+        strategy: this.findCommentStrategy,
+        component: SelectedText,
+      },
+    ]);
+
+    return { editorState: EditorState.createEmpty(decorator) };
   },
   getSelectionState() {
     const state = this.state;
@@ -20,11 +30,7 @@ const TextEditor = React.createClass({
     console.log('SELECTION STATE:', selectionState);
     console.log('BLOCK KEY:', selectionState.getAnchorKey());
   },
-  boldClick() {
-    this.handleChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
-  },
   handleChange(editorState) {
-    console.log(editorState);
     this.setState({ editorState });
   },
   createCommentEntity() {
@@ -34,13 +40,14 @@ const TextEditor = React.createClass({
     const contentStateWithEntity = contentState.createEntity('COMMENT', 'MUTABLE');
     const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
 
-
+    // New contentstate with comment entitiy attached
     const contentStateWithComment = Modifier.applyEntity(
       contentState,
       selectionState,
       entityKey,
     );
 
+    // New editorstate with comment entity attached
     const newEditorState = EditorState.push(
       editorState,
       contentStateWithComment,
@@ -48,26 +55,66 @@ const TextEditor = React.createClass({
     );
 
     this.setState({ editorState: newEditorState });
+    console.log(this.state.editorState);
+  },
+  resolveComment() {
+    const editorState = this.state.editorState;
+    const selectionState = editorState.getSelection();
+    const contentState = editorState.getCurrentContent();
+    const entityKey = null;
 
-    console.log(convertToRaw(newEditorState.getCurrentContent()));
+    // Removes a specific comment entity from the contentstate
+    const newContentState = Modifier.applyEntity(
+      contentState,
+      selectionState,
+      entityKey,
+    );
+
+    // New editorstate with the entity removed
+    const newEditorState = EditorState.push(
+      editorState,
+      newContentState,
+      'apply-entity',
+    );
+
+    this.setState({ editorState: newEditorState });
   },
   logState() {
     const editorState = this.state.editorState;
 
     console.log(convertToRaw(editorState.getCurrentContent()));
   },
+  findCommentStrategy(contentBlock, callback, contentState) {
+    contentBlock.findEntityRanges((character) => {
+      const entityKey = character.getEntity();
+
+      // If character is not part of an entity then ignore
+      if (entityKey === null) {
+        return false;
+      }
+
+      return contentState.getEntity(entityKey).getType() === 'COMMENT';
+    }, callback);
+  },
+  focus() {
+    return this.editor.focus();
+  },
   render() {
     return (
       <div id="content">
         <h1>Draft.js Editor</h1>
 
-        <div className="editor">
-          <Editor editorState={this.state.editorState} onChange={this.handleChange} />
-        </div>
-        <button onClick={this.boldClick}>Bold</button>
         <button onClick={this.getSelectionState}>Log Selection State</button>
         <button onClick={this.createCommentEntity}>Comment</button>
+        <button onClick={this.resolveComment}>Resolve</button>
         <button onClick={this.logState}>Log State</button>
+        <div className="editor" onClick={this.focus}>
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.handleChange}
+            ref={ref => this.editor = ref}
+          />
+        </div>
       </div>
     );
   },
