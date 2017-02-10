89b0d6559920a6679039a9a9153227be26d13ef2
Carlos Martinez <martinezcj2@gmail.com>
2017-02-10 09:31:49 -0500

    make sign up route on frontend

--- frontend/components/index.js
+++ frontend/components/index.js
@@ -2,6 +2,6 @@ import LandingPage from './landing-page/LandingPage';
 import CreatePage from './create-page/CreatePage';
 import Navbar from './Navbar';
 import BrowsePage from './browse-page/BrowseContainer';
+import SignUp from './landing-page/SignUp';
 
-export { LandingPage, Navbar, BrowsePage, CreatePage };
-
+export { LandingPage, Navbar, BrowsePage, CreatePage, SignUp };
