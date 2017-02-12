0f29f298ee443c1931ddf2e3539e107ab5d4fb4e
Ruben Gil <gil.r419@gmail.com>
2017-02-12 16:16:22 -0500

    remove trailing comma

--- frontend/components/document-page/SelectedText.jsx
+++ frontend/components/document-page/SelectedText.jsx
@@ -5,7 +5,7 @@ const SelectedText = (props) => {
     backgroundColor: '#ffa593',
     padding: '2px 0',
     border: '#ffa593 solid 1px',
-    borderRadius: '3px',
+    borderRadius: '3px'
   };
 
   return <span style={style}>{props.children}</span>;
