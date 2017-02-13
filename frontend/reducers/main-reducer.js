6f5d90ab97aeea5a095eff480f417e0bb1ab9df8
Carlos Martinez <martinezcj2@gmail.com>
2017-02-13 14:47:26 -0500

    remove category route

--- frontend/reducers/main-reducer.js
+++ frontend/reducers/main-reducer.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import user from './user-reducer';
 import browse from './browse-reducer';
-import category from './category-reducer';
 
 const mainReducer = combineReducers({
   user,
-  browse,
-  category,
+  browse
 });
 
 export default mainReducer;
