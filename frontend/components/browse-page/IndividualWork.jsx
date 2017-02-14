d561a5b6a6831c07c67d0b4cfab2f9d283abb137
Carlos Martinez <martinezcj2@gmail.com>
2017-02-14 02:16:21 -0500

    add withRouter

--- frontend/components/browse-page/IndividualWork.jsx
+++ frontend/components/browse-page/IndividualWork.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 
 const IndividualWork = React.createClass({
+  handleClick() {
+    const id = this.props.document.id;
+
+    this.props.router.push(`/document/${id}`);
+  },
   render() {
+    console.log(this.props)
     return (
-      <div className="individual-work">
+      <div className="individual-work" onClick={this.handleClick}>
         <h2>{this.props.document.title}</h2>
         <h3>{this.props.document.User.username}</h3>
         <p>{this.props.document.deadline}</p>
@@ -14,4 +21,4 @@ const IndividualWork = React.createClass({
   },
 });
 
-export default IndividualWork;
+export default withRouter(IndividualWork);
