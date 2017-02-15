e2fa6e3f9098a328c682c54fb5c4af40a3a8f684
jondubya86 <jonathanwu86@gmail.com>
2017-02-15 17:19:11 -0500

    add AccountPage route

--- frontend/components/Routes.jsx
+++ frontend/components/Routes.jsx
@@ -3,7 +3,7 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from '../store/store';
 
-import { LandingPage, Navbar, BrowsePage, CreatePage, DocumentPage, MyAccount, SignUp } from './index';
+import { LandingPage, Navbar, BrowsePage, CreatePage, DocumentPage, AccountPage, SignUp } from './index';
 
 const Routes = () => (
   <Provider store={store}>
@@ -11,7 +11,7 @@ const Routes = () => (
       <Route component={LandingPage} path="/signin" />
       <Route path="/" component={Navbar}>
         <IndexRoute component={BrowsePage} />
-        {/* <Route component={} path="account" /> */}
+        <Route component={AccountPage} path="account" />
         <Route component={CreatePage} path="create" />
         <Route component={SignUp} path="signup" />
         {/* <Route component={} path="document/:id" /> */}
