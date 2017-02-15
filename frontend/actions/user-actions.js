7e52d57ac3b2bdbc53165e4c82d980ad2deb9fb0
jondubya86 <jonathanwu86@gmail.com>
2017-02-14 23:46:32 -0500

    add edit user action

--- frontend/actions/user-actions.js
+++ frontend/actions/user-actions.js
@@ -1,18 +1,35 @@
 import { ajax } from 'jquery';
 
 export const GET_USER_DATA = 'GET_USER_DATA';
+export const EDIT_USER_DATA = 'EDIT_USER_DATA';
 
 const getUserData = payload => ({
   type: GET_USER_DATA,
   data: payload,
 });
 
-export const getUserDataAsync = () => (dispatch) => {
+export const getUserDataAsync = id => (dispatch) => {
   ajax({
-    url: '/api/users/1',
+    url: `/api/users/${id}`,
     type: 'GET',
   })
   .done((userData) => {
     dispatch(getUserData(userData));
+  });
+};
+
+const editUserData = payload => ({
+  type: EDIT_USER_DATA,
+  data: payload,
+});
+
+export const editUserDataAsync = (data, id) => (dispatch) => {
+  ajax({
+    url: `/api/users/${id}`,
+    type: 'PUT',
+    data,
+  })
+  .done((userData) => {
+    dispatch(editUserData(userData));
   });
 };
