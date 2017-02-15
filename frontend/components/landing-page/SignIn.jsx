d9e54cc463a99614045d7125f682053aed758df1
Carlos Martinez <martinezcj2@gmail.com>
2017-02-15 12:20:28 -0500

    remove trailing comma

--- frontend/components/landing-page/SignIn.jsx
+++ frontend/components/landing-page/SignIn.jsx
@@ -9,8 +9,8 @@ const SignIn = React.createClass({
       <div className="sign-in">
         <h1>Welcome. <span className="please-login">Please login.</span></h1>
         <form>
-          <input className="sign-up-input" type="email" placeholder="Email" />
-          <input className="sign-up-input" type="password" placeholder="Password" />
+          <input className="sign-in-input" type="email" placeholder="Email" />
+          <input className="sign-in-input" type="password" placeholder="Password" />
           <div className="sign-in-bottom">
             <p>Don{"'"}t have account? <span>Sign up</span></p>
             <input className="sign-in-button" type="submit" />
@@ -18,7 +18,7 @@ const SignIn = React.createClass({
         </form>
       </div>
     );
-  },
+  }
 });
 
 export default SignIn;
