f14749db44c89a894ac8f3aebd27eeb639dcc08a
Ruben Gil <gil.r419@gmail.com>
2017-02-14 19:17:00 -0500

    create comment-seeds file

---
+++ backend/seeders/comment-seeds.js
@@ -0,0 +1,69 @@
+const commentSeeds = [
+  {
+    comment: 'Bruh! What does this mean? I think you need a title thatbetter suits the writing',
+    anchor_key: 'cqvfr',
+    anchor_offset: 70,
+    focus_key: 'cqvfr',
+    focus_offset: 11,
+    is_backward: true,
+    has_focus: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    UserId: 4,
+    DocumentId: 1
+  },
+  {
+    comment: 'Great sentence',
+    anchor_key: '41dmf',
+    anchor_offset: 441,
+    focus_key: '41dmf',
+    focus_offset: 592,
+    is_backward: false,
+    has_focus: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    UserId: 4,
+    DocumentId: 1
+  },
+  {
+    comment: 'You mispelled this word, i believe you mean \'present\'',
+    anchor_key: '41dmf',
+    anchor_offset: 1633,
+    focus_key: '41dmf',
+    focus_offset: 1625,
+    is_backward: true,
+    has_focus: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    UserId: 4,
+    DocumentId: 1
+  },
+  {
+    comment: 'I believe you should expand on this. Give more details',
+    anchor_key: '8d69b',
+    anchor_offset: 4,
+    focus_key: '8d69b',
+    focus_offset: 346,
+    is_backward: false,
+    has_focus: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    UserId: 4,
+    DocumentId: 1
+  },
+  {
+    comment: 'Now this is an ending worth my cereal!',
+    anchor_key: '10gda',
+    anchor_offset: 0,
+    focus_key: '10gda',
+    focus_offset: 1492,
+    is_backward: false,
+    has_focus: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    UserId: 4,
+    DocumentId: 1
+  }
+]
+
+module.exports = commentSeeds;
