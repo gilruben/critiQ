77c0ebcc72b7cb8ad3a3be875f6b0ccf6417704d
jondubya86 <jonathanwu86@gmail.com>
2017-02-14 23:38:19 -0500

    create account redux reducer

--- frontend/reducers/account-reducer.js
+++ frontend/reducers/account-reducer.js
@@ -1,14 +1,12 @@
-import { GET_USER_ACCOUNT, EDIT_USER_ACCOUNT, DELETE_USER_ACCOUNT } from '../actions/user-actions';
+import { GET_USER_ACCOUNT, EDIT_USER_ACCOUNT } from '../actions/user-actions';
 
-const defaultState = { account: [] };
+const defaultState = { account: null };
 
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
     case GET_USER_ACCOUNT:
       return Object.assign({}, state, { account: action.data });
     case EDIT_USER_ACCOUNT:
-      return Object.assign({}, state, { account: action.data });
-    case DELETE_USER_ACCOUNT:
       return Object.assign({}, state, { account: action.data });
     default:
       return state;
