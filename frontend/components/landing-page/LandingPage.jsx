3ceea28fa5ddeadd135a137cc2957f3b47254f7b
Carlos Martinez <martinezcj2@gmail.com>
2017-02-15 13:16:09 -0500

    change 'final' to 'last'

--- frontend/components/landing-page/LandingPage.jsx
+++ frontend/components/landing-page/LandingPage.jsx
@@ -7,7 +7,7 @@ const LandingPage = React.createClass({
       <div className="landing-page">
         <div className="title-div">
           <img src="http://i.imgur.com/M1mRZc5.png" />
-          <h3 className="landing-motto">Make your first draft, your final</h3>
+          <h3 className="landing-motto">Make your first draft, your last</h3>
         </div>
         <SignIn />
       </div>
