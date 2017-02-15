152e2c189e5bd61ab28119bdd14421e362b5738e
jondubya86 <jonathanwu86@gmail.com>
2017-02-14 23:44:04 -0500

    user reducer now includes documents

--- frontend/reducers/user-reducer.js
+++ frontend/reducers/user-reducer.js
@@ -1,11 +1,21 @@
-import { GET_USER_DATA } from '../actions/user-actions';
+import { GET_USER_DATA, EDIT_USER_DATA } from '../actions/user-actions';
 
-const defaultState = { username: '', email: '', bio: '', rating: 0, level: '' };
+const defaultState = { id: null, username: '', email: '', bio: '', rating: 0, level: '', documents: [] };
 
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
     case GET_USER_DATA:
-      return Object.assign({}, state, action.data);
+      let { id, username, email, bio, rating, level } = action.data;
+      const documents = action.data.Documents;
+
+      return Object.assign({}, state, { id, username, email, bio, rating, level, documents });
+    case EDIT_USER_DATA:
+      username = action.data.username;
+      email = action.data.email;
+      bio = action.data.bio;
+      level = action.data.level;
+
+      return Object.assign({}, state, { username, email, bio, level });
     default:
       return state;
   }
