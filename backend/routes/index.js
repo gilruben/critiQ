f03a06e47da3b292523d227ca6707a22b9743cba
Crazy Kiwi <Nasita-Haque>
2017-02-16 15:56:25 -0500

    add authentication route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+// API Route: /api
 router.use('/users', require('./user-router'));
 router.use('/documents', require('./document-router'));
 router.use('/comments', require('./comment-router'));
+
+// Authentication Route: /auth
+router.use('/auth', require('./auth-router'));
 
 module.exports = router;
