3346530b7d58a35091fcbbb89119d7fd39432cbd
Crazy Kiwi <Nasita-Haque>
2017-02-16 17:40:02 -0500

    add verification function to onenter

--- frontend/components/Routes.jsx
+++ frontend/components/Routes.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from '../store/store';
-import { LandingPage, Navbar, BrowsePage, CreatePage, DocumentPage, SignUp, ProfilePage } from './index';
+import { LandingPage, Navbar, BrowsePage, CreatePage, DocumentPage, SignUp, ProfilePage, Verification } from './index';
 
 const Routes = () => (
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route component={LandingPage} path="/signin" />
-      <Route path="/" component={Navbar}>
+      <Route path="/" component={Navbar} onEnter={Verification} >
         <IndexRoute component={BrowsePage} />
         {/* <Route component={} path="account" /> */}
         <Route component={CreatePage} path="create" />
