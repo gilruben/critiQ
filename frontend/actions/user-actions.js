afa413b8ea9c47d9276bc8b588c796951dad2b72
Carlos Martinez <martinezcj2@gmail.com>
2017-02-16 09:57:31 -0500

    frontend/actions/user-actions.js

--- frontend/actions/user-actions.js
+++ frontend/actions/user-actions.js
@@ -4,13 +4,29 @@ export const GET_USER_DATA = 'GET_USER_DATA';
 
 const getUserData = payload => ({
   type: GET_USER_DATA,
-  data: payload,
+  data: payload
 });
 
 export const getUserDataAsync = () => (dispatch) => {
   ajax({
     url: '/api/users/1',
-    type: 'GET',
+    type: 'GET'
+  })
+  .done((userData) => {
+    dispatch(getUserData(userData));
+  });
+};
+
+export const postUserDataAsync = () => (dispatch) => {
+  ajax({
+    url: '/api/users/',
+    type: 'POST',
+    data: {
+      username: '',
+      password: '',
+      email: '',
+      level: ''
+    }
   })
   .done((userData) => {
     dispatch(getUserData(userData));
