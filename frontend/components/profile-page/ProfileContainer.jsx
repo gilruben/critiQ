0db7e65cd08cacc8b931334576895cc2e0b1a92d
jondubya86 <jonathanwu86@gmail.com>
2017-02-16 14:38:34 -0500

    fix syntax and rename function for clarity

--- frontend/components/profile-page/ProfileContainer.jsx
+++ frontend/components/profile-page/ProfileContainer.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { ajax } from 'jquery';
 
 const ProfileContainer = React.createClass({
   componentDidMount() {
+    const id = this.props.params.id;
+    ajax({
+      url: `/api/users/${id}`,
+      type: 'GET'
+    })
+    .done((userData) => {
+      this.setState(userData);
+    });
   },
   render() {
     return (
       <div>Hello World!</div>
     );
-  },
+  }
 });
 
 export default ProfileContainer;
