b04948d8792ca0734c4d9465a068e13964f34e03
jondubya86 <jonathanwu86@gmail.com>
2017-02-15 19:26:07 -0500

    add edit document status for user profile component

--- frontend/reducers/user-reducer.js
+++ frontend/reducers/user-reducer.js
@@ -1,12 +1,14 @@
 import { GET_USER_DATA, EDIT_USER_DATA } from '../actions/user-actions';
+import { EDIT_DOCUMENT_STATUS } from '../actions/document-actions';
 
 const defaultState = { id: null, username: '', email: '', bio: '', rating: 0, level: '', documents: [] };
 
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
     case GET_USER_DATA:
-      let { id, username, email, bio, rating, level } = action.data;
-      const documents = action.data.Documents;
+      const { rating } = action.data;
+      let { id, username, email, bio, level } = action.data;
+      let documents = action.data.Documents;
 
       return Object.assign({}, state, { id, username, email, bio, rating, level, documents });
     case EDIT_USER_DATA:
@@ -16,6 +18,15 @@ const reducer = (state = defaultState, action) => {
       level = action.data.level;
 
       return Object.assign({}, state, { username, email, bio, level });
+    case EDIT_DOCUMENT_STATUS:
+      documents = state.documents;
+      id = action.data.id;
+
+      const newDocs = documents.map(doc => (
+        (doc.id === id) ? action.data : doc
+      ));
+
+      return Object.assign({}, state, { documents: newDocs });
     default:
       return state;
   }
