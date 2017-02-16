bbca5dff588359d7d00f17518ba41f71e27a7883
jondubya86 <jonathanwu86@gmail.com>
2017-02-16 18:56:14 -0500

    create profile container for public profile view

--- frontend/components/profile-page/ProfileContainer.jsx
+++ frontend/components/profile-page/ProfileContainer.jsx
@@ -1,7 +1,16 @@
+// dependancies
 import React from 'react';
 import { ajax } from 'jquery';
+// css
+import '../../styles/account-page.css';
+// components
+import UserInfo from '../account-page/UserInfo';
+import ActiveDocs from './ActiveDocs';
 
 const ProfileContainer = React.createClass({
+  getInitialState() {
+    return ({ account: null });
+  },
   componentDidMount() {
     const id = this.props.params.id;
     ajax({
@@ -9,12 +18,22 @@ const ProfileContainer = React.createClass({
       type: 'GET'
     })
     .done((userData) => {
-      this.setState(userData);
+      this.setState({ account: userData });
     });
   },
   render() {
+    if (this.state.account) {
+      return (
+        <div>
+          <UserInfo account={this.state.account} />
+          <ActiveDocs username={this.state.account.username} activeList={this.state.account.Documents} />
+        </div>
+      );
+    }
     return (
-      <div>Hello World!</div>
+      <div>
+        Loading...
+      </div>
     );
   }
 });
