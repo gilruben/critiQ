c4e5e3e1fdf0f16154ca56aa8276085d9e251012
Carlos Martinez <martinezcj2@gmail.com>
2017-02-16 09:55:14 -0500

    add onclick function to categories

--- frontend/components/browse-page/CategoriesContainer.js
+++ frontend/components/browse-page/CategoriesContainer.js
@@ -8,6 +8,11 @@ const CategoryContainer = React.createClass({
 
     this.props.getDocuments(category, level);
   },
+  handleCategory(e) {
+    const category = e.target.innerHTML.toLowerCase().split(' ').join('_');
+
+    this.props.getDocuments(category)
+  },
   render() {
     const categories = ['Resume', 'Cover letters', 'Upcoming Deadlines', 'Other writings'];
     return (
@@ -15,16 +20,16 @@ const CategoryContainer = React.createClass({
         <ul>
           <li className="dropdown">
             <div className="dropbutton indiv-category">Essays</div>
-              <div className="dropdown-content">
-                <div className="level-div" onClick={this.handleEssay}>Middle School</div>
-                <div className="level-div" onClick={this.handleEssay}>High School</div>
-                <div className="level-div" onClick={this.handleEssay}>College</div>
-                <div className="level-div" onClick={this.handleEssay}>Other</div>
-              </div>
+            <div className="dropdown-content">
+              <div className="level-div" onClick={this.handleEssay}>Middle School</div>
+              <div className="level-div" onClick={this.handleEssay}>High School</div>
+              <div className="level-div" onClick={this.handleEssay}>College</div>
+              <div className="level-div" onClick={this.handleEssay}>Other</div>
+            </div>
           </li>
           {
             categories.map((ele, idx) => {
-              return <li className="indiv-category" onClick={this.handleEssay} key={idx}>{ele}</li>;
+              return <li className="indiv-category" onClick={this.handleCategory} key={idx}>{ele}</li>;
             })
           }
         </ul>
