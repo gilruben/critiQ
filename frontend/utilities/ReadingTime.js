d016616ebf55c0d93f1cacb00c1df9b016991291
Crazy Kiwi <Nasita-Haque>
2017-02-16 11:43:21 -0500

    clean up ReadingTime function

--- frontend/utilities/ReadingTime.js
+++ frontend/utilities/ReadingTime.js
@@ -5,19 +5,23 @@ const readingTime = (doc) => {
   const blockArray = [];
   let numOfMin = 0;
   let readTime = '';
+
   // Push the split single blocks into an array of subarrays.
   allBlocks.forEach((block) => {
     const singleBlock = block.text.split(' ');
     blockArray.push(singleBlock);
   });
+
   // Flatten the array of arrays.
   const flatBlockArray = blockArray.reduce((a, b) => (
      a.concat(b)
   ));
+
   // Filter flattened array and remove spaces.
   const finalBlockArray = flatBlockArray.filter(word => (
      word !== ' '
   ));
+
   // Find the average reading time of a body of text.
   // Average reading time for an adult is 250 WPM.
   if (finalBlockArray.length < 250) {
