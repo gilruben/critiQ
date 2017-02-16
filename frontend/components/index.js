3107b802a47228348c8b5e597a326c4f38009603
Crazy Kiwi <Nasita-Haque>
2017-02-16 17:42:51 -0500

    remove verification from index

--- frontend/components/index.js
+++ frontend/components/index.js
@@ -5,6 +5,5 @@ import BrowsePage from './browse-page/BrowseContainer';
 import SignUp from './landing-page/SignUp';
 import DocumentPage from './document-page/DocumentContainer';
 import ProfilePage from './profile-page/ProfileContainer';
-import Verification from '../utilities/Verification';
 
-export { LandingPage, Navbar, BrowsePage, CreatePage, DocumentPage, SignUp, ProfilePage, Verification };
+export { LandingPage, Navbar, BrowsePage, CreatePage, DocumentPage, SignUp, ProfilePage };
