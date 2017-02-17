72d1ce68e9951f4cc2d0a77a71d12026eda87880
Crazy Kiwi <Nasita-Haque>
2017-02-17 14:22:36 -0500

    clean up Verification

--- frontend/utilities/Verification.js
+++ frontend/utilities/Verification.js
@@ -1,8 +1,9 @@
 import { ajax } from 'jquery';
 
 // AJAX to check user login status on every page clicked.
-// If user fails, redirect to signin page otherwise continue to selected page. 
-const Verification = (nextstate, replace, cb) => {
+// If user fails, redirect to signin page otherwise continue to selected page.
+// Because callback is declared in params, cb must be included in promises to end function call.
+const verification = (nextstate, replace, cb) => {
   ajax({
     url: '/auth/verify',
     type: 'GET'
@@ -16,4 +17,4 @@ const Verification = (nextstate, replace, cb) => {
   });
 };
 
-export default Verification;
+export default verification;
