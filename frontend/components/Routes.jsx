2e5480e3dfb6012a61354c0f5dc82537477fc7fd
Crazy Kiwi <Nasita-Haque>
2017-02-17 14:22:16 -0500

    clean up routes

--- frontend/components/Routes.jsx
+++ frontend/components/Routes.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from '../store/store';
-import Verification from '../utilities/Verification';
+import verification from '../utilities/Verification';
 import { LandingPage, Navbar, BrowsePage, CreatePage, DocumentPage, SignUp, ProfilePage } from './index';
 
 const Routes = () => (
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route component={LandingPage} path="/signin" />
-      <Route path="/" component={Navbar} onEnter={Verification} >
+      <Route path="/" component={Navbar} onEnter={verification} >
         <IndexRoute component={BrowsePage} />
         {/* <Route component={} path="account" /> */}
         <Route component={CreatePage} path="create" />
