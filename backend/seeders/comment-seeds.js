732b07d851985fe7a23ab85ba13d442dd237f166
Ruben Gil <gil.r419@gmail.com>
2017-02-18 18:45:19 -0500

    add more comments to comment seed file

--- backend/seeders/comment-seeds.js
+++ backend/seeders/comment-seeds.js
@@ -63,6 +63,97 @@ const commentSeeds = [
     updatedAt: new Date(),
     UserId: 4,
     DocumentId: 1
+  },
+  {
+    comment: 'This needs to be centered',
+    anchorKey: '89fhq',
+    anchorOffset: 45,
+    focusKey: '89fhq',
+    focusOffset: 73,
+    isBackward: false,
+    hasFocus: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    UserId: 1,
+    DocumentId: 1
+  },
+  {
+    comment: 'You mispelled present',
+    anchorKey: '41dmf',
+    anchorOffset: 1625,
+    focusKey: '41dmf',
+    focusOffset: 1633,
+    isBackward: false,
+    hasFocus: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    UserId: 1,
+    DocumentId: 1
+  },
+  {
+    comment: 'This needs to be centered',
+    anchorKey: 'cf801',
+    anchorOffset: 70,
+    focusKey: 'cf801',
+    focusOffset: 48,
+    isBackward: true,
+    hasFocus: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    UserId: 1,
+    DocumentId: 1
+  },
+  {
+    comment: 'This sentence does add any value to the paragraph',
+    anchorKey: 'bl3d6',
+    anchorOffset: 958,
+    focusKey: 'bl3d6',
+    focusOffset: 1118,
+    isBackward: false,
+    hasFocus: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    UserId: 1,
+    DocumentId: 1
+  },
+  {
+    comment: 'This needs to be centered',
+    anchorKey: '73f6g',
+    anchorOffset: 73,
+    focusKey: '73f6g',
+    focusOffset: 32,
+    isBackward: true,
+    hasFocus: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    UserId: 1,
+    DocumentId: 1
+  },
+  {
+    comment: 'This needs to be centered',
+    anchorKey: '9ou1n',
+    anchorOffset: 70,
+    focusKey: '9ou1n',
+    focusOffset: 64,
+    isBackward: true,
+    hasFocus: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    UserId: 1,
+    DocumentId: 1
+  },
+  {
+    comment: 'Yasss! great conclusion!!',
+    anchorKey: '10gda',
+    anchorOffset: 0,
+    focusKey: '10gda',
+    focusOffset: 1492,
+    isBackward: false,
+    hasFocus: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    UserId: 1,
+    DocumentId: 1
   }
 ]
 
