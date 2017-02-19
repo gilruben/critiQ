60171d525da1e1980d6634e8a0cb84a1bf47a06a
Carlos Martinez <martinezcj2@gmail.com>
2017-02-19 12:11:01 -0500

    change file extension to jsx

---
+++ frontend/components/browse-page/BrowseContainer.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getDocumentsAsync } from '../../actions/browse-actions';
+import Categories from './CategoriesContainer';
+import IndividualWork from './IndividualWork';
+
+const BrowseContainer = React.createClass({
+  componentDidMount() {
+    this.props.getDocuments();
+  },
+  render() {
+    return (
+      <div className="browse-container">
+        <Categories getDocuments={this.props.getDocuments} />
+        <div className="documents-list-div">
+          {
+            this.props.browse.documents.map((docs, idx) => {
+              console.log(docs)
+              return (
+                <div key={idx}>
+                  <IndividualWork document={docs} />
+                </div>
+              );
+            })
+          }
+        </div>
+      </div>
+    );
+  },
+});
+
+const mapStateToProps = (state) => {
+  return { browse: state.browse };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ getDocuments: getDocumentsAsync }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BrowseContainer);
