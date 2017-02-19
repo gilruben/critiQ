be276e73ed0daaf6eaf2b37a385678294af42c4b
Carlos Martinez <martinezcj2@gmail.com>
2017-02-19 15:20:13 -0500

    remove console log

--- frontend/utilities/Deadline.js
+++ frontend/utilities/Deadline.js
@@ -1,9 +1,8 @@
 import moment from 'moment';
 
 const deadline = (date) => {
-  const newDate = date.slice(0, 10);
-  console.log(newDate)
-  return moment(newDate).endOf('day').fromNow();
+  const deadlineDate = date.slice(0, 10);
+  return moment(deadlineDate).endOf('day').fromNow();
 };
 
 export default deadline;
