69ecf4fbcba22e864e84adcf1bb9334e831a889c
Ruben Gil <gil.r419@gmail.com>
2017-02-19 13:57:19 -0500

    create resolve button and call resolve function on click

--- frontend/components/document-page/CommentContainer.jsx
+++ frontend/components/document-page/CommentContainer.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
-const CommentContainer = React.createClass({
-  render() {
-    return (
+const CommentContainer = (props) => {
+  const { comment, resolver } = props;
+  const reviewer = comment.User;
+  const { anchorKey, anchorOffset, focusKey, focusOffset, isBackward, hasFocus, id } = comment;
+  const commentData = { anchorKey, anchorOffset, focusKey, focusOffset, isBackward, hasFocus };
+
+  return (
+    <div>
       <div>
-        {this.props.comment.comment}
+        {reviewer.username}
+        <button onClick={resolver.bind(null, commentData, id)}>resolve</button>
       </div>
-    );
-  }
-});
+      <div>{comment.comment}</div>
+    </div>
+  );
+};
 
 export default CommentContainer;
