5138c0cdf145c55f8e186b5a86f131246d5d97ab
Ruben Gil <gil.r419@gmail.com>
2017-02-20 13:27:55 -0500

    create AddComment component

---
+++ frontend/components/document-page/AddComment.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddComment = (props) => {
+  return (
+    <div>
+      <button id="add-comment-button"> + </button>
+    </div>
+  );
+};
+
+export default AddComment;
