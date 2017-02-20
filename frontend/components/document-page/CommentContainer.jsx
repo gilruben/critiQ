982f95698ab1863267cf7d1f0a58d8c70200f1b6
Ruben Gil <gil.r419@gmail.com>
2017-02-20 13:09:08 -0500

    remove commentData from resolve function

--- frontend/components/document-page/CommentContainer.jsx
+++ frontend/components/document-page/CommentContainer.jsx
@@ -3,14 +3,13 @@ import React from 'react';
 const CommentContainer = (props) => {
   const { comment, resolver } = props;
   const reviewer = comment.User;
-  const { anchorKey, anchorOffset, focusKey, focusOffset, isBackward, hasFocus, id } = comment;
-  const commentData = { anchorKey, anchorOffset, focusKey, focusOffset, isBackward, hasFocus };
+  const { id } = comment;
 
   return (
     <div>
       <div>
         {reviewer.username}
-        <button onClick={resolver.bind(null, commentData, id)}>resolve</button>
+        <button onClick={resolver.bind(null, id)}>resolve</button>
       </div>
       <div>{comment.comment}</div>
     </div>
