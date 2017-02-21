bae83614aa77cc7931505ed371a90520c546a1bf
Nasita-Haque <nhaque@fandm.edu>
2017-02-21 15:33:36 -0500

    remove auth router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,7 +7,4 @@ router.use('/users', require('./user-router'));
 router.use('/documents', require('./document-router'));
 router.use('/comments', require('./comment-router'));
 
-// Authentication Route: /auth
-router.use('/auth', require('./auth-router'));
-
 module.exports = router;
