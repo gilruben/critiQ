a47464a0f1fd5a594d59b7b06f870437d7be3cb6
Nasita-Haque <nhaque@fandm.edu>
2017-02-21 15:52:39 -0500

    remove verification and signup route

--- frontend/components/Routes.jsx
+++ frontend/components/Routes.jsx
@@ -3,13 +3,13 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from '../store/store';
 import verification from '../utilities/Verification';
-import { LandingPage, Navbar, BrowsePage, CreatePage, DocumentPage, SignUp, ProfilePage } from './index';
+import { LandingPage, Navbar, BrowsePage, CreatePage, DocumentPage, ProfilePage } from './index';
 
 const Routes = () => (
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route component={LandingPage} path="/signin" />
-      <Route path="/" component={Navbar} onEnter={verification} >
+      <Route path="/" component={Navbar}>
         <IndexRoute component={BrowsePage} />
         {/* <Route component={} path="account" /> */}
         <Route component={CreatePage} path="create" />
