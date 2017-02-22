fb6b04735bd7d76993354b0556b78d7f128ba7b7
Carlos Martinez <martinezcj2@gmail.com>
2017-02-22 11:18:52 -0500

    add the ellipsis

--- frontend/components/browse-page/IndividualWork.jsx
+++ frontend/components/browse-page/IndividualWork.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { withRouter } from 'react-router';
 import readingTime from '../../utilities/ReadingTime';
 import deadline from '../../utilities/Deadline';
+import characterCount from '../../utilities/CharacterCount';
 
 const IndividualWork = React.createClass({
   handleClick() {
@@ -21,7 +22,7 @@ const IndividualWork = React.createClass({
           <span>•</span>
           <p className="individual-work-name">{this.props.document.User.username}</p>
         </div>
-        <p className="snippet">{this.props.document.body.blocks[0].text}</p>
+        <p className="snippet">{characterCount(this.props.document) + "..."}</p>
         <div className="dead-rev">
           <p className="deadline">ends {deadline(this.props.document.deadline)}</p>
           <p className="rev"># <i className="fa fa-user" /></p>
