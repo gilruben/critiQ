c0bba0b8be15a403c2c48f67d18791e27c265f80
Ruben Gil <gil.r419@gmail.com>
2017-02-23 16:23:34 -0500

    call dispatch selectComment action  when CommentContainer is clicked

--- frontend/components/document-page/CommentContainer.jsx
+++ frontend/components/document-page/CommentContainer.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
+
 const CommentContainer = (props) => {
-  const { comment, resolver, selectedComment } = props;
+  const { comment, resolver, selectedComment, selectComment } = props;
   const reviewer = comment.User;
   const { id } = comment;
   const commentClass = selectedComment === id ? 'user-resolve-div-selected' :
     'user-resolve-div';
+  const commentData = { commentId: id };
 
   return (
-    <div className="comment-box">
+    <div className="comment-box" onClick={selectComment.bind(null, commentData)}>
       <div className={commentClass}>
         <p>{reviewer.username}</p>
         <button onClick={resolver.bind(null, id)}>resolve</button>
