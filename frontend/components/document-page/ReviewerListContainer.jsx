819b47372bc6ae4e5e61031001f24b313fcd341b
Carlos Martinez <martinezcj2@gmail.com>
2017-02-23 14:49:59 -0500

    Pass selectedReviewer into reviewer

--- frontend/components/document-page/ReviewerListContainer.jsx
+++ frontend/components/document-page/ReviewerListContainer.jsx
@@ -7,13 +7,12 @@ const ReviewerListContainer = React.createClass({
   },
   render() {
     const reviewers = Object.keys(this.props.reviewers);
-
     return (
       <ul className="reviewer-list-ul">
         {
           reviewers.map(reviewer => (
             <li key={reviewer} onClick={this.handleClick.bind(this, reviewer)}>
-              <Reviewer username={reviewer} />
+              <Reviewer username={reviewer} selectedReviewer={this.props.selectedReviewer} />
             </li>
           ))
         }
