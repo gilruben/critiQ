bc4bd61276fe35691f81391c20898d5e96feaae3
Ruben Gil <gil.r419@gmail.com>
2017-02-23 13:27:25 -0500

    add functionality to allow SelectedText to change color when selected

--- frontend/components/document-page/SelectedText.jsx
+++ frontend/components/document-page/SelectedText.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 
 // This component is used to highlight the selected text targeted for commenting
 const SelectedText = (props) => {
+  const { contentState, entityKey } = props;
+  const entity = contentState.getEntity(entityKey);
+  const { commentId, selectedComment, selectComment } = entity.getData();
+  const commentData = { commentId };
+
   const style = {
-    backgroundColor: '#ffa593',
+    backgroundColor: selectedComment === commentId ? '#ff7b60' : '#ffa593',
     padding: '2px 0',
     border: '#ffa593 solid 1px',
     borderRadius: '3px'
   };
-  const { contentState, entityKey } = props;
-  const entity = contentState.getEntity(entityKey);
-  // console.log(entity.getData());
-  return <span style={style}>{props.children}</span>;
+
+  return (
+    <span style={style} onClick={selectComment.bind(null, commentData)}>
+      {props.children}
+    </span>
+  );
 };
 
 export default SelectedText;
