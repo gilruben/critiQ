632c1349ac3861d63a3886e3d7108c4736dfd127
Carlos Martinez <martinezcj2@gmail.com>
2017-02-23 16:34:05 -0500

    Add destructured variable

--- frontend/components/document-page/ReviewerListContainer.jsx
+++ frontend/components/document-page/ReviewerListContainer.jsx
@@ -7,12 +7,13 @@ const ReviewerListContainer = React.createClass({
   },
   render() {
     const reviewers = Object.keys(this.props.reviewers);
+    const { selectedReviewer } = this.props;
     return (
       <ul className="reviewer-list-ul">
         {
           reviewers.map(reviewer => (
             <li key={reviewer} onClick={this.handleClick.bind(this, reviewer)}>
-              <Reviewer username={reviewer} selectedReviewer={this.props.selectedReviewer} />
+              <Reviewer username={reviewer} selectedReviewer={selectedReviewer} />
             </li>
           ))
         }
