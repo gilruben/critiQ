35169c2a9b48d9e52f345ae20a145e8e283b5c7d
jondubya86 <jonathanwu86@gmail.com>
2017-02-24 20:20:33 -0500

    add logo and css to 404 page

--- frontend/components/404-page/ErrorPage.jsx
+++ frontend/components/404-page/ErrorPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import '../../styles/404-page.css';
 
 const ErrorPage = () => {
   return (
-    <div className="error-page">
-      <h1>404 Page</h1>
-      <h3>You seem lost. Hoo are you looking for?</h3>
+    <div className="error">
+      <img id="img" src="http://i.imgur.com/bkVx6UG.png" />
+      <div className="error-text">
+        <div className={'error-title'}>
+          <div>404 Page</div>
+        </div>
+        <div className={'error-msg'}>You seem lost. Hoo are you looking for?</div>
+      </div>
     </div>
   );
 };
