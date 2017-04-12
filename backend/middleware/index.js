7dc5e7b809dd1aff16212ca3d4aa66760a1325c6
Ruben Gil <gil.r419@gmail.com>
2017-04-12 12:56:58 -0400

    add connect-pg-simple session store

--- backend/middleware/index.js
+++ backend/middleware/index.js
@@ -1,6 +1,9 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const PgSimpleSessionStore = require('connect-pg-simple')(session);
+const passportConfig = require('../passport/passport');
 const path = require('path');
 
 // Function for adding middleware
@@ -8,11 +11,22 @@ const applyExpressMiddleware = (app) => {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(express.static(path.join(__dirname, '../../frontend/bundle')));
+  app.use(cookieParser());
   app.use(session({
+    store: new PgSimpleSessionStore({
+      conString: 'postgres://ruben@localhost:5432/critiq'
+    }),
     secret: 'English Master Manipulator',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24 * 90 // 90 days
+    }
   }));
+
+  passportConfig(app);
 };
 
 module.exports = applyExpressMiddleware;
