27dbc7a01c975387aed03a09a20cc7b9db569b17
Ruben Gil <gil.r419@gmail.com>
2017-04-20 14:12:37 -0400

    remove unnecessary comments

--- backend/passport/passport.js
+++ backend/passport/passport.js
@@ -1,7 +1,5 @@
 const passport = require('passport');
-// const localStrategy = require('./strategies/local.strategy');
 const jwtStrategy = require('./strategies/jwt.strategy');
-// const User = require('../models').User;
 
 const passportConfig = (app) => {
   // Passport
