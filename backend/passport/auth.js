e03efdd5dc1f0fd1054325076c327bfdb5130b39
Ruben Gil <gil.r419@gmail.com>
2017-04-20 17:10:43 -0400

    add authenticate function

---
+++ backend/passport/auth.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+const tokenFuncs = () => {
+  // const secret = 'English Master Manipulator';
+  // const options = {
+  //   audience: 'litclub.herokuapp.com',
+  //   issuer: 'critiq',
+  //   expiresIn: '90 days'
+  // };
+
+  const sign = (payload) => {
+    const secret = 'English Master Manipulator';
+    const options = {
+      audience: 'litclub.herokuapp.com',
+      issuer: 'critiq',
+      expiresIn: '90 days'
+    };
+
+    if (!payload && typeof payload !== 'object' && !Array.isArray(payload)) {
+      return new Error('Payload must be an object');
+    }
+
+    return jwt.sign(payload, secret, options);
+  };
+
+ /**
+ * @param {String} strategy - The passport strategy to use
+ * @param {Object} options - Any options to pass to the authenticate function
+ *
+ * @return {Function} a closure with the strategy and option saved in the function
+ */
+  const authenticate = (strategy, options) => {
+    const opts = options || {};
+
+    return passport.authenticate(strategy, opts);
+  };
+
+  return { sign, authenticate };
+};
+
+module.exports = tokenFuncs();
