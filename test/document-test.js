aa87da8a94fef178bb0aadea448b2fe3329e238a
Ruben Gil <gil.r419@gmail.com>
2017-05-13 19:04:44 -0400

    Add test for authenticated users in document test

--- test/document-test.js
+++ test/document-test.js
@@ -1,115 +1,178 @@
 const expect = require('chai').expect;
 const supertest = require('supertest');
+const moment = require('moment');
 const server = require('../backend/server');
+const agent = supertest.agent(server);
+const documentSeeds = require('../backend/seeders/document-seeds');
 const Document = require('../backend/models').Document;
 
 describe('document-api-test', () => {
-  // Fake document data that we'll use for tests
-  const documents = [
-    { title: 'No Spear, Just Shake', body: { foo: 'bar' }, category: 'essay', privacy: 'public', deadline: new Date(Date.UTC(2017, 4, 20, 3, 0, 0)), active: true, UserId: 1 },
-    { title: 'Dude, I Lost My For-Loop', body: { foo: 'bar' }, category: 'essay', privacy: 'private', deadline: new Date(Date.UTC(2017, 3, 20, 3, 0, 0)), active: true, UserId: 2 },
-    { title: 'When Bad and Bourgeois Meets Good and Humble', body: { foo: 'bar' }, category: 'resume', privacy: 'public', deadline: new Date(Date.UTC(2017, 4, 17, 3, 0, 0)), active: false, UserId: 3 },
-    { title: 'Did Tom Brady Make A Deal With Darth Pence?!', body: { foo: 'bar' }, category: 'essay', privacy: 'public', deadline: new Date(Date.UTC(2017, 4, 13, 3, 0, 0)), active: true, UserId: 1 },
-  ];
+  let testDocumentId;
 
-
-  before(() => Document.sync({ force: true })
-    .then(() => Document.bulkCreate(documents))
-    .catch(err => console.log('DB Err!', err)));
-
-  // Test to get all documents route
-  it('\'/api/documents\' should respond with all public and active documents', (done) => {
-    supertest(server)
-      .get('/api/documents')
-      .end((err, res) => {
-        expect(res.body.length).equal(2);
-
-        done();
-      });
-  });
-
-
-  // Test to create a new document
-  it('\'/api/documents\' should respond with the document created', (done) => {
-    const newDocument = { title: 'Infinite Looo-', body: { foo: 'bar' }, category: 'resume', privacy: 'public', deadline: new Date(Date.UTC(2017, 4, 17, 3, 0, 0)), active: true, UserId: 1 };
-
-    supertest(server)
-      .post('/api/documents')
-      .send(newDocument)
-      .end((err, res) => {
-        expect(res.body.title).equal(newDocument.title);
-        expect(res.body.body).eql(newDocument.body);
-        expect(res.body.category).equal(newDocument.category);
-        expect(res.body.privacy).equal(newDocument.privacy);
-        expect(res.body.active).equal(newDocument.active);
-
-        done();
-      });
-  });
-
-  // Test GET query with category
-    it('\'/api/documents\' should respond with category query', (done) => {
-    supertest(server)
-      .get('/api/documents?category=resume')
-      .end((err, res) => {
-        expect(res.body.length).equal(1);
-        expect(res.body[0].id).equal(5);
-
-        done();
-      });
-  });
-
-  // Test to get a specific document
-  it('\'/api/documents/:id\' should respond with a specific document', (done) => {
-    supertest(server)
-    .get('/api/documents/1')
+  // Logs in user
+  it('\'/auth/login\' will log in the user', (done) => {
+    agent
+    .post('/auth/login')
+    .send({ email: 'nhaque@gmail.com', password: 'password' })
     .end((err, res) => {
-      expect(res.body.id).equal(1);
-      expect(res.body.title).equal(documents[0].title);
-      expect(res.body.body).eql(documents[0].body);
-      expect(res.body.category).equal(documents[0].category);
-      expect(res.body.privacy).equal(documents[0].privacy);
-      expect(res.body.active).equal(documents[0].active);
+      done();
+    });
+  });
+
+
+  // Test to get all documents
+  it('\'/api/documents\' should respond with all documents that are public and active', (done) => {
+    agent
+    .get('/api/documents')
+    .end((err, res) => {
+      expect(res.body.length).equal(5);
 
       done();
     });
   });
 
-  // Test to create a new document with validation error for body.
-  it('\'/api/documents\' should respond with an error', (done) => {
-    const newDocument = { title: 'That Time I NPMed No-Demon', body: 'foobar', category: 'essay', privacy: 'public', deadline: new Date(Date.UTC(2016, 11, 20, 3, 0, 0)), active: true, UserId: 2 };
 
+  // Test to have an authenticated user get all documents
+  it('\'/api/documents\' should respond with all documents that are public and active', (done) => {
     supertest(server)
-      .post('/api/documents')
-      .send(newDocument)
-      .end((err, res) => {
-        expect(res.body.body).eql(newDocument.body);
+    .get('/api/documents')
+    .end((err, res) => {
+      expect(res.status).equal(401);
 
-        done();
-      });
+      done();
+    });
   });
 
-  // Test to delete single document by id.
-  it('\'/api/documents/:id\' should respond an object', (done) => {
 
-    supertest(server)
-      .delete('/api/documents/4')
-      .end((err, res) => {
-        expect(res.body).eql({ documentsDeleted: 1 });
+  const deadline = moment().format().slice(0, 10)
+  const newDocument = { title: 'Infinite Looo-', body: documentSeeds[4], category: 'resume', privacy: 'public', deadline, active: true};
 
-        done();
-      });
+  // Test to create a new document
+  it('\'/api/documents\' should respond with the document created', (done) => {
+    agent
+    .post('/api/documents')
+    .send(newDocument)
+    .end((err, res) => {
+      expect(res.body.title).equal(newDocument.title);
+      expect(res.body.body).eql(newDocument.body);
+      expect(res.body.category).equal(newDocument.category);
+      expect(res.body.privacy).equal(newDocument.privacy);
+      expect(res.body.active).equal(newDocument.active);
+
+      testDocumentId = res.body.id;
+
+      done();
+    });
   });
+
+
+  // Test to have an authenticated user create a new document
+  it('\'/api/documents\' should respond with the document created', (done) => {
+    supertest(server)
+    .post('/api/documents')
+    .send(newDocument)
+    .end((err, res) => {
+      expect(res.status).equal(401);
+
+      done();
+    });
+  });
+
+
+  // Test to get a specific document
+  it('\'/api/documents/:id\' should respond with a specific document', (done) => {
+    agent
+    .get(`/api/documents/${testDocumentId}`)
+    .end((err, res) => {
+      expect(res.body.id).equal(testDocumentId);
+      expect(res.body.title).equal(newDocument.title);
+      expect(res.body.body).eql(newDocument.body);
+      expect(res.body.category).equal(newDocument.category);
+      expect(res.body.privacy).equal(newDocument.privacy);
+      expect(res.body.active).equal(newDocument.active);
+
+      done();
+    });
+  });
+
+
+  // Test to have an unauthenticated user get a specific document
+  it('\'/api/documents/:id\' should respond with a specific document', (done) => {
+    supertest(server)
+    .get(`/api/documents/${testDocumentId}`)
+    .end((err, res) => {
+      expect(res.status).equal(401);
+
+      done();
+    });
+  });
+
+
+  // Test to create a new document with incorrect type on body field.
+  it('\'/api/documents\' should respond with an array with an error message', (done) => {
+    const newDocument = { title: 'That Time I NPMed No-Demon', body: 'foobar', category: 'essay', privacy: 'public', deadline, active: true };
+
+    agent
+    .post('/api/documents')
+    .send(newDocument)
+    .end((err, res) => {
+      expect(res.status).equal(400);
+      expect(res.body).eql({ errorMessages: ['The body field must be a JSON object'] });
+
+      done();
+    });
+  });
+
+
+  // Test to create a new document with invalid value on privacy field.
+  it('\'/api/documents\' should respond with an array with an error message', (done) => {
+    const newDocument = { title: 'That Time I NPMed No-Demon', body: documentSeeds[2], category: 'essay', privacy: 'secret', deadline, active: true };
+
+    agent
+    .post('/api/documents')
+    .send(newDocument)
+    .end((err, res) => {
+      expect(res.status).equal(400);
+      expect(res.body).eql({ errorMessages: ['Invalid value passed to privacy field'] });
+
+      done();
+    });
+  });
+
 
   // Test to update single document by id.
   it('\'/api/documents/:id\' should respond with new body data', (done) => {
-    const newData = { body: { foo: 'bar2' } };
+    const newData = { body: documentSeeds[1]};
 
-    supertest(server)
-    .put('/api/documents/1')
+    agent
+    .put(`/api/documents/${testDocumentId}`)
     .send(newData)
     .end((err, res) => {
-      expect(res.body).eql(newData.body);
+      expect(res.body.body).eql(documentSeeds[1]);
+
+      done();
+    });
+  });
+
+
+  // Test GET query with category
+  it('\'/api/documents\' should respond with category query', (done) => {
+    agent
+    .get('/api/documents?category=essay')
+    .end((err, res) => {
+      expect(res.body.length).equal(4);
+
+      done();
+    });
+  });
+
+
+  // Test to delete single document by id.
+  it('\'/api/documents/:id\' should respond with an object containing the number of documents deleted', (done) => {
+    agent
+    .delete(`/api/documents/${testDocumentId}`)
+    .end((err, res) => {
+      expect(res.body).eql({ documentsDeleted: 1 });
 
       done();
     });
