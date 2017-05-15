1b2f4f28b74803f0a94c33174ff302ba4e5e6994
Ruben Gil <gil.r419@gmail.com>
2017-05-14 22:33:53 -0400

    Create deadline checker to handle expired documents

--- backend/utilities/deadline-checker.js
+++ backend/utilities/deadline-checker.js
@@ -2,20 +2,26 @@ const Document = require('../models').Document;
 const CronJob = require('cron').CronJob;
 const moment = require('moment');
 
-const deadlineCheck = new CronJob({
-  cronTime: '00 29 11 * * 1-7',
-  onTick() {
-    // Get yesterdays date
-    let dateToSetInactive = moment().subtract(1, 'day');
-    dateToSetInactive = dateToSetInactive.format().slice(0, 10);
+const deadlineCheck = (deadlineStr) => {
+  const checker = new CronJob({
+    cronTime: deadlineStr,
+    onTick() {
+      // Get yesterday's date
+      let dateToSetInactive = moment().subtract(1, 'day');
+      dateToSetInactive = dateToSetInactive.format().slice(0, 10);
 
-    Document.update({ active: false }, {
-      where: {
-        deadline: dateToSetInactive
-      }
-    });
-  },
-  timeZone: 'America/New_York'
-});
+      // Set the active field of all documents, with a deadline of yesterday's
+      // date, to false
+      Document.update({ active: false }, {
+        where: {
+          deadline: dateToSetInactive
+        }
+      });
+    },
+    timeZone: 'America/New_York'
+  });
+
+  checker.start();
+};
 
 module.exports = deadlineCheck;
