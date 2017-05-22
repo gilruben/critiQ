007adb6757a66d3792dc45eabf8394575bd8278e
Ruben Gil <gil.r419@gmail.com>
2017-05-22 13:26:42 -0400

    Add function to redirect users away from signin page if they are already logged in

--- frontend/utilities/Verification.js
+++ frontend/utilities/Verification.js
@@ -1,3 +1,4 @@
+import { ajax } from 'jquery';
 import { dispatch } from '../store/store';
 import { getUserDataAsync } from '../actions/user-actions';
 
@@ -6,8 +7,22 @@ import { getUserDataAsync } from '../actions/user-actions';
 // dispatching the getUserDataAsync action. If the user is logged in, the data
 // will be retrieved and the user will be allowed to access the requested route.
 // If the user was not logged in, they will be redirected to the signin page.
-const verification = (nextstate, replace, cb) => {
+export const verification = (nextstate, replace, cb) => {
   dispatch(getUserDataAsync(replace, cb));
 };
 
-export default verification;
+// Redirects the user away from the signin page if they are logged in. If they
+// are logged in then they will be taken to the account page.
+export const redirectFromSignIn = (nextstate, replace, cb) => {
+  ajax({
+    url: '/auth/verify',
+    type: 'GET'
+  })
+  .done(() => {
+    replace('/');
+    cb();
+  })
+  .fail(() => {
+    cb();
+  });
+};
