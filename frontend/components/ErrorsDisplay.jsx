312c5e681111cc58cc519d707eb56679074d63f8
Ruben Gil <gil.r419@gmail.com>
2017-07-19 00:07:37 -0400

    Return null in ErrorsDisplay if there are no error messages

--- frontend/components/ErrorsDisplay.jsx
+++ frontend/components/ErrorsDisplay.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 const ErrorsDisplay = (props) => {
   const errorMsgs = props.errorMsgs || [];
 
+  if (!errorMsgs.length) return null;
+
   return (
     <ul className="error-list">
       {errorMsgs.map((msg, indx) => <li key={indx} className="error-list-item">{msg}</li>)}
