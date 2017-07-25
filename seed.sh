0a4b0c966b5d8d4984f2e5dddef52aa0f05fc609
gilruben <gil.r419@gmail.com>
2017-07-25 16:09:23 -0400

    Add the path to the sequelize executable

--- seed.sh
+++ seed.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sequelize db:seed --seeders-path ./backend/seeders --config ./backend/config/config.json --seed ./backend/seeders/20170214155739-user-seeder
+./node_modules/.bin/sequelize db:seed --seeders-path ./backend/seeders --config ./backend/config/config.json --seed ./backend/seeders/20170214155739-user-seeder
 node ./backend/seeders/document-seeder
-sequelize db:seed --seeders-path ./backend/seeders --config ./backend/config/config.json --seed ./backend/seeders/20170214231329-comment-seeder
+./node_modules/.bin/sequelize db:seed --seeders-path ./backend/seeders --config ./backend/config/config.json --seed ./backend/seeders/20170214231329-comment-seeder
